51ac4a8501f96f441ce740179ecf888ebf0c1b33
Matt Wright <matt@nobien.net>
2011-06-08 18:52:52 -0400

    changed language or parameters to match those used by Instagram

--- spring-social-instagram/src/main/java/org/springframework/social/instagram/connect/InstagramConnectionFactory.java
+++ spring-social-instagram/src/main/java/org/springframework/social/instagram/connect/InstagramConnectionFactory.java
@@ -5,8 +5,8 @@ import org.springframework.social.instagram.api.Instagram;
 
 public class InstagramConnectionFactory extends OAuth2ConnectionFactory<Instagram> {
 
-	public InstagramConnectionFactory(String consumerKey, String consumerSecret) {
-		super("instagram", new InstagramServiceProvider(consumerKey, consumerSecret), new InstagramAdapter());
+	public InstagramConnectionFactory(String clientId, String clientSecret) {
+		super("instagram", new InstagramServiceProvider(clientId, clientSecret), new InstagramAdapter());
 	}
 	
 }
